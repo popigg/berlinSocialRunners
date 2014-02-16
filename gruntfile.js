0b56b6dd499a338381a31e882aaea8d706af4467
popigg <popigg@gmail.com>
2014-02-16 23:58:01 +0100

    change git ignore to add the grunfile to git

---
+++ gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {		
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-ngmin');
+
+	grunt.initConfig({
+		ngmin: {
+			controllers: {
+    				src: [
+    					'js/app.js', 
+    					'js/controllers.js'
+    					],
+				    dest: 'js/all.min.js'
+			},												
+		},		
+		concat: {
+	        options: {
+	            separator: '',
+	        },
+	        dist: {
+	            src: [
+	            	'bower_components/angular/angular.min.js',
+	                'bower_components/angular-route/angular-route.min.js',
+	                'bower_components/angular-bootstrap/ui-bootstrap.min.js',
+	                'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+	                'js/all.min.js',	                
+	            ],
+	            dest: 'js/main.min.js',
+	        },        
+	    },
+	    copy: {
+			main: {			    
+			    expand: true,
+			    cwd: 'bower_components/bootstrap/dist/fonts/',
+			    src: '**',
+			    dest: 'fonts/',			    
+			    filter: 'isFile'
+		  	},
+	    },
+	    cssmin: {	    	
+	    	minify: {	    			
+				    src: 'css/style.css',
+				    dest: 'css/style.min.css',				    
+			},
+			combine: {
+			    files: {
+			      'css/main.min.css': [
+			      			'bower_components/bootstrap/dist/css/bootstrap.min.css', 
+			      			'bower_components/bootstrap/dist/css/bootstrap-theme.min.css', 
+			      			'css/style.min.css'
+  					]
+			    }
+			}
+		},
+	});	
+
+	grunt.registerTask('default',['ngmin', 'concat', 'cssmin']);
+};
